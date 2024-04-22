85d448d98f99e1ac661059ea5a61fd02bf8560fd
EricJansen <EricJansen@LAPTOP-EJ>
2024-04-23 00:53:04 +0200

    Cleaned up code.

--- Vmk.Infrastructure/Configuration/BgServiceIntervalOptions.cs
+++ Vmk.Infrastructure/Configuration/BgServiceIntervalOptions.cs
@@ -1,17 +1,10 @@
 ﻿namespace Vmk.Infrastructure.Configuration;
 
+#nullable disable
+
 public class BgServiceIntervalOptions
 {
-    public int Value { get; init; } = 0;
+    public int Value { get; init; }
 
-    public IntervalBetweenOptions ValueBetween { get; init; } = new();
-
-}
-
-
-public class IntervalBetweenOptions
-{
-    public int Min { get; init; } = 0;
-
-    public int Max { get; init; } = 0;
+    public BgIntervalBetweenOptions ValueBetween { get; init; } = new();
 }
